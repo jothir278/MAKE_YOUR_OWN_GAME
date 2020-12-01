76f9d0c80535cc238b6dbc6d26f689447cac9ddd
jothir278 <jothiradithyagraju@gmail.com>
2020-12-01 20:04:45 +0530

    almost done

--- sketch.js
+++ sketch.js
@@ -2,37 +2,71 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
-var engine, world;
-var ground, ball;
+var car,carImage;
+var bg,bgImg;
+var ground,groundImg;
+var coins,coinsImg;
+
+function preload(){
+    carImage = loadImage("images/allVehiles19.png");
+    bgImg = loadImage("images/bg.png");
+    groundImg = loadImage("images/ground.png");
+    coinsImg = loadImage("images/coins.png");
+}
 
 function setup(){
-    var canvas = createCanvas(400,400);
+    var canvas = createCanvas(displayWidth,displayHeight);
     engine = Engine.create();
     world = engine.world;
+
+    car = Bodies.rectangle(600,100,100,100);
+    carImage = loadImage("images/allvehiles19.png");
+    World.add(world,car);
+    
+    
 
     var ground_options ={
         isStatic: true
     }
 
-    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    ground = Bodies.rectangle(600,600,400,10,ground_options);
+    groundImg = loadImage("images/ground.png");
     World.add(world,ground);
-
-    var ball_options ={
-        restitution: 1.0
-    }
-
-    ball = Bodies.circle(200,100,20, ball_options);
-    World.add(world,ball);
 
     console.log(ground);
 }
 
 function draw(){
-    background(0);
+    background(bgImg);
     Engine.update(engine);
-    rectMode(CENTER);
-    rect(ground.position.x,ground.position.y,400,20);
+    imageMode(CENTER)
+    image(groundImg,ground.position.x,ground.position.y,400,20);
+    image(carImage,car.position.x,car.position.y,35,35);
 
-    ellipseMode(RADIUS);
-    ellipse(ball.position.x, ball.position.y, 20, 20);
+    spawnCoins();
+
+    if(keyPressed("right")){
+        car.velocityX = 9;
+    }
+
+    if(keyCode===27){
+        car.velocityX = -9;
+    }
+
+    if(keyDown("SPACE")){
+        car.velocityX = 0;
+    }
+
+    drawSprites();
+}
+
+function spawnCoins(){
+if(frameCount%20 === 0){
+coins = createSprite(1300,200,10,10);
+coinsImg = loadImage("images/coins.png");
+coins.addImage(coinsImg);
+coins.velocityX = -4;
+coins.y = Math.round(random(10,700));
+World.add(world,coins);
+    }
 }
